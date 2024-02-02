38b16ba74e7d905b4be04761cc0f0ab747bfe513
magus96 <anirudh5462@gmail.com>
2024-02-02 17:26:53 +0530

    Refactor

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func irradiate(duration int, radMode string) int {
 	} else {
 		rad = 10
 	}
+	time.Sleep(time.Duration(duration) * time.Second)
 	return rad * duration
 }
 
@@ -27,6 +28,5 @@ func main() {
 	time.Sleep(2 * time.Second)
 	go setMode("X")
 	rad := irradiate(10, mode)
-	time.Sleep(10 * time.Second)
 	fmt.Printf("You were irradiated with %d rads!!!\n", rad)
 }
